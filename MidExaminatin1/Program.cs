b7d28166f60257480ce0106db0835b321f4b280d
ITRC <ITRC@SL070801>
2024-03-25 11:04:18 +0530

    commit maths problem

--- MidExaminatin1/Program.cs
+++ MidExaminatin1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int total = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                total = total + numbers[i];
+
+            }
+            double average = total / 10;
+            Console.WriteLine("total = " + total);
+            Console.WriteLine("average = " + average);
         }
     }
 }
