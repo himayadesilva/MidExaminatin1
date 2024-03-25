7d8eb14ce463a142951883bc994742b2096a4750
ITRC <ITRC@SL070801>
2024-03-25 11:18:53 +0530

    commit again

---
+++ MidExaminatin1/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidExamination1
+{
+    internal class Employee
+    {
+        // create properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BasicSalary { get; set; }
+
+        //create methods
+        public void print()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + ", Salary: " + BasicSalary);
+        }
+        
+
+    }
+}
--- MidExaminatin1/Program.cs
+++ MidExaminatin1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MidExaminatin1
+﻿using MidExamination1;
+
+namespace MidExaminatin1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee employee1 = new Employee();
+
+            employee1.print();
         }
     }
 }
