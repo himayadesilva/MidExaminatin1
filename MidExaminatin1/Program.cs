52effd950d4ce63ffc4f7a33b62779c5dea2c2b1
ITRC <ITRC@SL070801>
2024-03-25 10:55:50 +0530

    Add project files.

---
+++ MidExaminatin1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MidExaminatin1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
